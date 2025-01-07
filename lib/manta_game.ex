252e5ef0a83d18e42b8f009b7bb5209616ad2e7c
SergioMir018 <sergiommirs@gmail.com>
2025-01-06 21:35:07 -0500

    first commit

---
+++ lib/manta_game.ex
@@ -0,0 +1,10 @@
+defmodule MantaGame do
+  @moduledoc """
+  Main module for MantaGame.
+  """
+  def start do
+    IO.puts("Bienvenido a Manta!")
+    IO.puts("Pulsa ENTER para comenzar...")
+    IO.gets("")
+  end
+end
---
+++ test/manta_game_test.exs
@@ -0,0 +1,8 @@
+defmodule MantaGameTest do
+  use ExUnit.Case
+  doctest MantaGame
+
+  test "greets the world" do
+    assert MantaGame.hello() == :world
+  end
+end
